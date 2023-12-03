e4f4003325abf5239cc40f3aedc4e9851ae1026e
Sergey <sergsmile29@gmail.com>
2023-12-03 23:06:40 +0200

    init State pr

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import { Header } from './components/Header'
-import dataArticle from './assets/article.json'
-import { Statistics } from './components/Statistics'
-import dataStats from './assets/data.json'
-import { BlogCardStyled } from './components/BlogCard/BlogCardStyled'
-import { GlobalStyles } from './SharedUI/Global'
+// Приклад даних - [{ id: '1', title: 'React is so cool', completed: false },{ id: '2', title: 'JS', completed: true }]
+//Plan
+// 1. Створити компонент для TodoList
+// 2. Створити компонент для TodoItem
+// 3. Створити стейт для TodoList
+// 4. Прокинути дані в компонент TodoList
+// 5. Зробити метод для видалення todo
+// 6. Зробити метод для додавання todo
+// 7. Зробити метод для перемикання стану (active / completed)
+// 8. Додати фільтр по стану todo (all, active, completed)
+// 9. Додати підрахунок активних задач
 
 function App() {
-	return (
-		<div>
-			<Header title='Project-js' />
-			<BlogCardStyled {...dataArticle} />
-			<Statistics data={dataStats} />
-		</div>
-	)
+	return <div></div>
 }
 
 export default App
