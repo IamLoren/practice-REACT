9a060472131af1cdc3748835614d4cd73fce8c2d
Mary <32392607+MartFrida@users.noreply.github.com>
2023-12-12 14:02:52 +0100

    reducer

---
+++ src/reducer/productReducer.js
@@ -0,0 +1,19 @@
+export const initialState = {
+  productsData: [],
+  skip: 0,
+  loading: false,
+  total: 0,
+  isCartOpen: false,
+  cartContent: [],
+}
+
+export const productReducer = (state, action) => {
+  // // switch (key) {
+  // //   case value:
+
+  // //     break;
+
+  //   default:
+  //     return state;
+  // }
+}
