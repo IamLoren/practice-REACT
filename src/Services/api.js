75f05c7f9b51704a7e2c32a46fba3ce67d006bb3
”John <102276922+UlianaNad@users.noreply.github.com>
2023-12-14 15:31:16 +0200

    search

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,25 +1,25 @@
-import axios from 'axios'
+import axios from "axios";
 
-axios.defaults.baseURL = 'https://dummyjson.com/'
+axios.defaults.baseURL = "https://dummyjson.com/";
 
-export const fetchProduct = async configParams => {
-	const { data } = await axios.get('products', {
-		params: {
-			limit: 3,
-			...configParams,
-		},
-	})
+export const fetchProduct = async (configParams) => {
+  const { data } = await axios.get("products", {
+    params: {
+      limit: 3,
+      ...configParams,
+    },
+  });
 
-	return data
-}
+  return data;
+};
 
 // 'https://dummyjson.com/products/1'
-export const fetchProductById = async id => {
-	const { data } = await axios.get(`products/${id}`)
-	return data
-}
+export const fetchProductById = async (id) => {
+  const { data } = await axios.get(`products/${id}`);
+  return data;
+};
 
-export const fetchProductByQuery = async query => {
-	const { data } = await axios.get(`products/search?q=${query}`)
-	return data.products
-}
+export const fetchProductByQuery = async (query) => {
+  const { data } = await axios.get(`products/search?q=${query}`);
+  return data;
+};
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,51 +1,63 @@
-import React, { useCallback, useContext, useEffect, useReducer } from 'react'
-import { fetchProduct } from '../Services/api'
-import { initialState, productReducer } from '../reducer/productReducer'
-import { MyContext } from '../Context/ContextProvider'
-import { Header } from '../components/Header/Header'
-import { LoadMoreBtn } from '../components/LoadMoreBtn/LoadMoreBtn'
-import { LoginForm } from '../components/LoginForm/LoginForm'
-import { Loader } from '../components/Loader/Loader'
-import { CartContext } from '../Context/CartProvider'
-import { Products } from '../components/Products/Products'
+import React, { useCallback, useContext, useEffect, useReducer } from "react";
+import { fetchProduct, fetchProductByQuery } from "../Services/api";
+import { initialState, productReducer } from "../reducer/productReducer";
+import { MyContext } from "../Context/ContextProvider";
+import { Header } from "../components/Header/Header";
+import { LoadMoreBtn } from "../components/LoadMoreBtn/LoadMoreBtn";
+import { LoginForm } from "../components/LoginForm/LoginForm";
+import { Loader } from "../components/Loader/Loader";
+import { CartContext } from "../Context/CartProvider";
+import { Products } from "../components/Products/Products";
+import { handle } from "express/lib/application";
 
 const ProductsPage = () => {
-	const [state, dispatch] = useReducer(productReducer, initialState)
-	const { productsData, skip, loading, total } = state
+  const [state, dispatch] = useReducer(productReducer, initialState);
+  const { productsData, skip, loading, total } = state;
 
-	const getData = useCallback(async () => {
-		try {
-			dispatch({ type: 'SET_LOADING', payload: true })
-			const { products, total } = await fetchProduct({
-				limit: 10,
-				skip,
-			})
+  const getData = useCallback(async () => {
+    try {
+      dispatch({ type: "SET_LOADING", payload: true });
+      const { products, total } =
+        // (await fetchProductByQuery(value)) ||
+        await fetchProduct({
+          limit: 10,
+          skip,
+        });
 
-			dispatch({ type: 'FETCH_PRODUCT_DATA', payload: { products, total } })
-		} catch (error) {
-		} finally {
-			dispatch({ type: 'SET_LOADING', payload: false })
-		}
-	}, [skip])
+      dispatch({ type: "FETCH_PRODUCT_DATA", payload: { products, total } });
+    } catch (error) {
+    } finally {
+      dispatch({ type: "SET_LOADING", payload: false });
+    }
+  }, [skip]);
 
-	useEffect(() => {
-		getData()
-	}, [getData, skip])
+  useEffect(() => {
+    getData();
+  }, [getData, skip]);
 
-	const { isOnline } = useContext(MyContext)
+  const { isOnline } = useContext(MyContext);
 
-	const handleLoadMore = () => {
-		dispatch({ type: 'SET_SKIP' })
-	}
+  const handleLoadMore = () => {
+    dispatch({ type: "SET_SKIP" });
+  };
 
-	return (
-		<div>
-			<Products products={productsData} />
-			{total > productsData.length ? <LoadMoreBtn click={handleLoadMore} /> : null}
+  const handleSetQuery = (e) => {
+    // setValue(e.target.value);
+  };
+  return (
+    <div>
+      <div>
+        <input onChange={handleSetQuery} value={"23"} type="text" />
+        <button>Search</button>
+      </div>
+      <Products products={productsData} />
+      {total > productsData.length ? (
+        <LoadMoreBtn click={handleLoadMore} />
+      ) : null}
 
-			{loading ? <Loader /> : null}
-		</div>
-	)
-}
+      {loading ? <Loader /> : null}
+    </div>
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
