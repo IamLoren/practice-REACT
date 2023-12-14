09d99b13ede8a17ed045fc474856027254f68318
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-14 15:08:27 +0100

    task_ 03

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useContext, useEffect, useReducer } from "react";
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+  useState,
+} from "react";
 import { fetchProduct, fetchProductByQuery } from "../Services/api";
 import { initialState, productReducer } from "../reducer/productReducer";
 import { MyContext } from "../Context/ContextProvider";
@@ -8,28 +14,30 @@ import { LoginForm } from "../components/LoginForm/LoginForm";
 import { Loader } from "../components/Loader/Loader";
 import { CartContext } from "../Context/CartProvider";
 import { Products } from "../components/Products/Products";
-import { handle } from "express/lib/application";
+import { useSearchParams } from "react-router-dom";
 
 const ProductsPage = () => {
+  const [value, setValue] = useState("");
   const [state, dispatch] = useReducer(productReducer, initialState);
   const { productsData, skip, loading, total } = state;
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query") || "";
   const getData = useCallback(async () => {
     try {
       dispatch({ type: "SET_LOADING", payload: true });
-      const { products, total } =
-        // (await fetchProductByQuery(value)) ||
-        await fetchProduct({
-          limit: 10,
-          skip,
-        });
+      const { products, total } = query
+        ? await fetchProductByQuery(query)
+        : await fetchProduct({
+            limit: 10,
+            skip,
+          });
 
       dispatch({ type: "FETCH_PRODUCT_DATA", payload: { products, total } });
     } catch (error) {
     } finally {
       dispatch({ type: "SET_LOADING", payload: false });
     }
-  }, [skip]);
+  }, [skip, query]);
 
   useEffect(() => {
     getData();
@@ -42,13 +50,16 @@ const ProductsPage = () => {
   };
 
   const handleSetQuery = (e) => {
-    // setValue(e.target.value);
+    setValue(e.target.value);
+  };
+  const handleSearch = () => {
+    setSearchParams(value ? { query: value } : {});
   };
   return (
     <div>
       <div>
-        <input onChange={handleSetQuery} value={"23"} type="text" />
-        <button>Search</button>
+        <input onChange={handleSetQuery} value={value} type="text" />
+        <button onClick={handleSearch}>Search</button>
       </div>
       <Products products={productsData} />
       {total > productsData.length ? (
