9946721cf7b197f9e449af466b10ae10a75e3992
SVTroya <my.svtroy@gmail.com>
2023-12-15 15:35:54 +0200

    useReducer

--- src/Context/CartProvider.jsx
+++ src/Context/CartProvider.jsx
@@ -6,17 +6,44 @@ export const CartProvider = ({ children }) => {
 	const [cart, setCart] = useState([])
 
 	const handleAddToCart = product => {
-		setCart(prev => [...prev, product])
+		setCart(prev => [...prev, {...product, count:1}])
 	}
 	const handleDeleteFromCart = id => {
 		setCart(prev => prev.filter(el => el.id !== id))
 	}
-	const calculatePrice = () => cart.reduce((acc, product) => acc + product.price, 0)
+
+	const handleIncrement = (id) => {
+		setCart(prev => prev.map(product =>
+		{
+			if(product.id === id) {
+				return {...product, count: product.count + 1}
+			}
+			else {
+				return product
+			}
+		}))
+	}
+
+	const handleDecrement = (id) => {
+		setCart(prev => prev.map(product =>
+		{
+			if(product.id === id && product.count >= 2) {
+				return {...product, count: product.count - 1}
+			}
+			else {
+				return product
+			}
+		}))
+	}
+
+	const calculatePrice = () => cart.reduce((acc, product) => acc + product.price * product.count, 0)
 
 	const value = {
 		cart,
 		handleAddToCart,
 		handleDeleteFromCart,
+		handleIncrement,
+		handleDecrement,
 		total: calculatePrice(),
 	}
 
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,16 +1,16 @@
-import React, { useContext } from "react";
-import { RiDeleteBin2Line } from "react-icons/ri";
-import { styled } from "styled-components";
-import { CartContext } from "../../Context/CartProvider";
+import React, {useContext} from "react";
+import {RiDeleteBin2Line} from "react-icons/ri";
+import {CartContext} from "../../Context/CartProvider";
+import {CartProduct, DeleteBtn} from "./Cart.styled";
 
 const Cart = () => {
-  const { cart, total, handleDeleteFromCart } = useContext(CartContext);
+  const {cart, total, handleDeleteFromCart, handleIncrement, handleDecrement} = useContext(CartContext);
   return (
     <>
       <h1>Total Price:{total}$</h1>
       <ul>
         {cart.map((product) => (
-          <CartProduct>
+          <CartProduct key={product.id}>
             <img
               src={product.thumbnail}
               alt={product.title}
@@ -19,12 +19,17 @@ const Cart = () => {
             />
             <p>Title: {product.title}</p>
             <p>Price: {product.price}$</p>
+            <div>
+              <button onClick={() => handleDecrement(product.id)}>-</button>
+              <span>{product.count}</span>
+              <button onClick={() => handleIncrement(product.id)}>+</button>
+            </div>
 
             <DeleteBtn
               type="button"
               onClick={() => handleDeleteFromCart(product.id)}
             >
-              <RiDeleteBin2Line size={20} />
+              <RiDeleteBin2Line size={20}/>
             </DeleteBtn>
           </CartProduct>
         ))}
@@ -33,21 +38,4 @@ const Cart = () => {
   );
 };
 
-const CartProduct = styled.li`
-  display: flex;
-  gap: 15px;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const DeleteBtn = styled.button`
-  background-color: transparent;
-  border: none;
-
-  &:hover,
-  &:focus {
-    color: red;
-    transform: scale(1.3);
-  }
-`;
 export default Cart;
