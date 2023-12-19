68bd4abdd64076be156bac19e289484a20f43607
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-12-19 15:00:27 +0200

    persistor

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from "./App";
 import { GlobalStyles } from "./SharedUI/Global";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
       <GlobalStyles />
     </Provider>
   </>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,39 @@
-import { combineReducers, createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+import { combineReducers } from "redux";
 import { taskReducer } from "./tasks/slice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { configureStore } from "@reduxjs/toolkit";
 
-const devtools = devToolsEnhancer();
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  blacklist: ["filter"],
+};
+
+const persistedReducer = persistReducer(persistConfig, taskReducer);
 
 const rootReducer = combineReducers({
-  taskData: taskReducer,
+  taskData: persistedReducer,
 });
 
-export const store = createStore(rootReducer, devtools);
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
