c280c8761b15ebf6d22d08da0633148bf49cddb5
Alyonka556 <142902721+Alyonka556@users.noreply.github.com>
2023-12-21 13:25:36 +0100

    createSelect

--- src/components/TaskList/DataList.jsx
+++ src/components/TaskList/DataList.jsx
@@ -1,42 +1,55 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectFilter, selectTasks } from "../../redux/tasks/selectors";
-import { deleteTaskThunk, renameTaskThunk, toogleTasksThunk } from "../../redux/tasks/operations";
+import {
+  selectFilter,
+  selectFilteredTasks,
+  selectTasks,
+} from "../../redux/tasks/selectors";
+import {
+  deleteTaskThunk,
+  renameTaskThunk,
+  toogleTasksThunk,
+} from "../../redux/tasks/operations";
 
 export const DataList = () => {
+  const tasks = useSelector(selectFilteredTasks);
+
   const dispatch = useDispatch();
   const handleDeleteItem = (id) => {
     dispatch(deleteTaskThunk(id));
   };
 
-  const tasks = useSelector(selectTasks);
-  const filter = useSelector(selectFilter);
-  const getFiltered = () => {
-    switch (filter) {
-      case "active":
-        return tasks.filter((item) => !item.completed);
-      case "completed":
-        return tasks.filter((item) => item.completed);
-      default:
-        return tasks;
-    }
-  };
+  // const tasks = useSelector(selectTasks);
+  // const filter = useSelector(selectFilter);
+  // const getFiltered = () => {
+  //   switch (filter) {
+  //     case "active":
+  //       return tasks.filter((item) => !item.completed);
+  //     case "completed":
+  //       return tasks.filter((item) => item.completed);
+  //     default:
+  //       return tasks;
+  //   }
+  // };
 
   const handleRename = (task) => {
-    const updatedTask = { ...task, title: prompt('Enter new value') || task.title }
-    dispatch(renameTaskThunk(updatedTask)) 
-  }
+    const updatedTask = {
+      ...task,
+      title: prompt("Enter new value") || task.title,
+    };
+    dispatch(renameTaskThunk(updatedTask));
+  };
   return (
     <div>
       <ul>
-        {getFiltered().map((task) => (
+        {tasks.map((task) => (
           <li key={task.id}>
             <input
               type="checkbox"
               checked={task.completed}
               onChange={() => dispatch(toogleTasksThunk(task))}
             />
-            <p onClick={()=>handleRename(task)}>{task.title}</p>
+            <p onClick={() => handleRename(task)}>{task.title}</p>
             <button onClick={() => handleDeleteItem(task.id)}>Delette</button>
           </li>
         ))}
--- src/redux/tasks/selectors.js
+++ src/redux/tasks/selectors.js
@@ -1,4 +1,34 @@
-export const selectTasks = (state) => state.taskData.tasks
-export const selectFilter = (state) => state.taskData.filter
-export const selectIsLoading = (state) => state.taskData.isLoading
-export const selectError = (state) => state.taskData.error
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectTasks = (state) => state.taskData.tasks;
+export const selectFilter = (state) => state.taskData.filter;
+export const selectIsLoading = (state) => state.taskData.isLoading;
+export const selectError = (state) => state.taskData.error;
+
+export const selectFilteredTasks = createSelector(
+  [selectFilter, selectTasks],
+  (filter, tasks) => {
+    console.log("Filter");
+    switch (filter) {
+      case "active":
+        return tasks.filter((item) => !item.completed);
+      case "completed":
+        return tasks.filter((item) => item.completed);
+      default:
+        return tasks;
+    }
+  }
+);
+
+// export const selectFilteredTasks = (state) => {
+//   const filter = selectFilter(state);
+//   const tasks = selectTasks(state);
+//   switch (filter) {
+//     case "active":
+//       return tasks.filter((item) => !item.completed);
+//     case "completed":
+//       return tasks.filter((item) => item.completed);
+//     default:
+//       return tasks;
+//   }
+// };
