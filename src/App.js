36bd29a00df91e0e54fa4dd8d65b93153f6159e6
Sergey <sergsmile29@gmail.com>
2023-12-22 00:43:27 +0200

    create pr

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import { useDispatch } from 'react-redux'
 import { fetchTasksThunk } from './redux/tasks/operations'
 //Plan
 
-// Додати складні селектори
-// Перенести фільтрацію в селектори та зробити createSelector
-// Зробити підрахунок активних задач через createSelector
-// Заблокувати кнопку видалення коли йде запит
-
 const App = () => {
 	const dispatch = useDispatch()
-
 	useEffect(() => {
 		dispatch(fetchTasksThunk())
 	}, [dispatch])
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,26 +1,21 @@
-import React from "react";
-import { AddTaskForm } from "./AddTaskForm";
-import { FilterTask } from "./FilterTask";
-import { DataList } from "./DataList";
-import { useSelector } from "react-redux";
-import {
-  selectError,
-  selectIsLoading,
-  selectUncompliteTasks,
-} from "../../redux/tasks/selectors";
+import React from 'react'
+import { AddTaskForm } from './AddTaskForm'
+import { FilterTask } from './FilterTask'
+import { DataList } from './DataList'
+import { useSelector } from 'react-redux'
+import { selectError, selectIsLoading, selectUncompliteTasks } from '../../redux/tasks/selectors'
 
 export const TaskList = () => {
-  const isLoading = useSelector(selectIsLoading);
-  const error = useSelector(selectError);
-  const uncomplited = useSelector(selectUncompliteTasks);
-  return (
-    <div>
-      <AddTaskForm />
-      <FilterTask />
-      <p>You have to complite {uncomplited} tasks.</p>
-      <DataList />
-      {isLoading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-    </div>
-  );
-};
+	const isLoading = useSelector(selectIsLoading)
+	const error = useSelector(selectError)
+	const uncomplited = useSelector(selectUncompliteTasks)
+	return (
+		<div>
+			<AddTaskForm />
+			<FilterTask />
+			<p>You have to complite {uncomplited} tasks.</p>
+			<DataList />
+			{error && <p>Error: {error}</p>}
+		</div>
+	)
+}
