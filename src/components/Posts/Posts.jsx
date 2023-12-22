82e292d4de298978b6d4a2cb4bc8de19e029c5fa
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-12-22 15:44:28 +0200

    add logout

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import AddForm from "./AddForm";
 import AuthAuthor from "./AuthAuthor";
 import PostList from "./PostList";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/posts/authorSlice";
 // План
 // - Розбити по компонентам розмітку
 // - Додати слайс для постів
@@ -11,12 +13,26 @@ import PostList from "./PostList";
 // - Редагування
 
 export const Posts = () => {
+  const dispatch = useDispatch();
+  const isLog = useSelector((state) => state.author.isOnline);
+
   return (
     <div>
       {/* Форма додавання поста */}
       <AddForm />
       {/* Поле для авторизації автора */}
-      <AuthAuthor />
+      {!isLog ? (
+        <AuthAuthor />
+      ) : (
+        <button
+          onClick={() => {
+            dispatch(logout());
+          }}
+        >
+          Exit
+        </button>
+      )}
+
       {/* Пости з даними */}
       <PostList />
     </div>
