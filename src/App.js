4bf5f52def8e2a58becc1d3ddeec63b2b0610110
Sergey <sergsmile29@gmail.com>
2023-12-25 23:24:09 +0200

    create auth pr

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import React, { useEffect } from "react";
-import { TaskList } from "./components/TaskList/TaskList";
-import { useDispatch } from "react-redux";
-import { fetchTasksThunk } from "./redux/tasks/operations";
-import { Posts } from "./components/Posts/Posts";
+import { Posts } from './components/Posts/Posts'
+import { TaskList } from './components/TaskList/TaskList'
 //Plan
-
+// Підключити роутер до проекту (notFound, layout, index)
+// Конфігурувати аксіос, використати create
+// Створити слайс для auth і додати файл операцій
+// Додати можливість реєстрації та логінізації
+// Додати можливість для виходу з облікового запису
 const App = () => {
-  //   const dispatch = useDispatch();
-  //   useEffect(() => {
-  //     dispatch(fetchTasksThunk());
-  //   }, [dispatch]);
+	return (
+		<div>
+			<TaskList />
+		</div>
+	)
+}
 
-  return (
-    <div>
-      {/* <TaskList /> */}
-      <Posts />
-    </div>
-  );
-};
-
-export default App;
+export default App
--- src/components/Posts/AddForm.jsx
+++ src/components/Posts/AddForm.jsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
-import { addPost } from "../../redux/posts/postSlice";
-import { nanoid } from "nanoid";
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
+import { addPost } from '../../redux/posts/postSlice'
+import { nanoid } from 'nanoid'
 
 const AddForm = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch()
 
-  const { register, handleSubmit, reset } = useForm();
-  const onSubmit = (data) => {
-    dispatch(
-      addPost({ id: nanoid(), ...data, author: "Olexsander Sergienko" })
-    );
-    reset();
-  };
+	const { register, handleSubmit, reset } = useForm()
+	const onSubmit = data => {
+		dispatch(addPost({ id: nanoid(), ...data, author: 'Olexsander Sergienko' }))
+		reset()
+	}
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input {...register("title", { required: true })} />
-      <textarea {...register("body", { required: true })} />
-      <button>Add post</button>
-    </form>
-  );
-};
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<input {...register('title', { required: true })} />
+			<textarea {...register('body', { required: true })} />
+			<button>Add post</button>
+		</form>
+	)
+}
 
-export default AddForm;
+export default AddForm
