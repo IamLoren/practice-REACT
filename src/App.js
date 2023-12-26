881d0453051af20c615bafebd3ee693614b462be
Sergey <sergsmile29@gmail.com>
2023-12-26 23:27:12 +0200

    pr15_final

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { Route, Routes } from "react-router";
-import { Posts } from "./components/Posts/Posts";
-import { TaskList } from "./components/TaskList/TaskList";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
+import { Route, Routes } from 'react-router'
+import { TaskList } from './components/TaskList/TaskList'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Header from './components/Header'
 //Plan
-// Підключити роутер до проекту (notFound)
-// Конфігурувати аксіос, використати create
-// Створити слайс для auth і додати файл операцій
-// Додати можливість реєстрації та логінізації
-// Додати можливість для виходу з облікового запису
+// Додати форми логіну / регістрації
+// Додати рефреш
+// Додати заглушку при рефреш
+// Додати приватні маршрути
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/todos' element={<TaskList />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
+		</>
+	)
+}
 
-        <Route path="/todos" element={<TaskList />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
-};
-
-export default App;
+export default App
